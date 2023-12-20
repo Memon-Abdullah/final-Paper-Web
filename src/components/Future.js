4071826e3d7d720f474bbdb6080ad7b21369dd8b
Abdullah Memon <abdullahmemon.bscsf21@iba-suk.edu.pk>
2023-12-20 15:42:12 +0500

    hero section half

---
+++ src/components/Future.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../assets/style/future.css';
+
+export const Future =()=>{
+    return(
+        <>
+            <div className="container-fluid">
+                <div className="container">
+                    <div className="row">
+                            
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+} 
