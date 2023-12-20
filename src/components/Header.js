01bd1d2360dcc65fd50036cccc0901d4a731cf0d
Abdullah Memon <abdullahmemon.bscsf21@iba-suk.edu.pk>
2023-12-20 15:26:29 +0500

    header and footer is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,25 +4,27 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-
+import logo from '../assets/images/logo.png';
+import '../assets/style/header.css';
 export const Header = ()=> {
     return(
         <>
-            <Navbar expand="lg" className="bg-body-tertiary">
+            <Navbar expand="lg" className="bg-body-tertiary navbar">
                 <Container fluid>
-                    <Navbar.Brand href="#">CodeByAbdullah</Navbar.Brand>
+                    <Navbar.Brand href="#"><img className="img-fluid logo" src={logo} alt="logo"/></Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0"style={{ maxHeight: '300px' }} navbarScroll>
-                        <Nav.Link ><Link to={"/"}>Home</Link></Nav.Link>
-                        <Nav.Link ><Link to={"/About"}>About</Link></Nav.Link>
-                        <Nav.Link ><Link to={"/Contact"}>Contact</Link></Nav.Link> 
+                    <Nav className="ms-auto my-2 my-lg-0"style={{ maxHeight: '300px' }} navbarScroll>
+                        <Nav.Link ><Link className="links-header" to={"/"}>Web3Mkr</Link></Nav.Link>
+                        <Nav.Link ><Link className="links-header" to={"/About"}>White Paper</Link></Nav.Link>
+                        <Nav.Link ><Link className="links-header" to={"/Services"}>Services</Link></Nav.Link> 
+                        <Nav.Link ><Link className="links-header" to={"/Tool"}>Tool</Link></Nav.Link> 
+                        <Nav.Link ><Link className="links-header" to={"/Contact"}>Contact Us</Link></Nav.Link> 
                     </Nav>
         
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <h1>This is Header</h1>
         </>
     )
 }
